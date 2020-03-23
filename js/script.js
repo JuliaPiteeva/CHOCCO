8109f6606d9d35de414bc59a5992a0ae26d79950
Bubnova Julia <Bubnovayn@gmail.com>
2020-03-23 18:36:40 +0600

    menu аккордеон в секции menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Секция "hero" открытие и закрытие полноэкранного меню
 const hamburgerOpen = document.querySelector(".hamburger-link");
 var hamburgerMenu = document.querySelector(".hamburger-menu");
 hamburgerOpen.addEventListener("click", function(e) {
@@ -10,8 +11,34 @@ hamburgerClose.addEventListener("click", function() {
   hamburgerMenu.style.display = "none";
 });
 
+// Секция "slider" , кеопка состава батончика
+
 const nutritionLink = document.querySelector(".bar__nutrition-link");
 nutritionLink.addEventListener("click", function(event) {
   event.preventDefault();
   console.log("На данный момент работает только по hover");
 });
+
+// Секция "menu" аккордеон
+
+var menulinkArray = document.getElementsByClassName("menu__link");
+var menuContentArray = document.getElementsByClassName("menu__desc-content");
+
+for (var i = 0; i < menulinkArray.length; i++) {
+  menulinkArray[i].addEventListener("click", function(e) {
+    e.preventDefault();
+    var openDesc = this.nextElementSibling;
+    openDesc.classList.toggle("menu__desc-content--active");
+  });
+}
+
+var menuContentCloseArray = document.getElementsByClassName("close");
+for (var i = 0; i < menuContentCloseArray.length; i++) {
+  menuContentCloseArray[i].addEventListener("click", function(e) {
+    var closeDesc = this.parentNode;
+    var closeDesc2 = closeDesc.parentNode;
+    closeDesc2.classList.remove("menu__desc-content--active");
+  });
+}
+// Секция "team" аккордеон
+var teamLinkArray = document.getElementsByClassName("accordeon__title");
